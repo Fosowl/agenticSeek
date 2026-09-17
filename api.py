778cacf141e0202c0839370a2174bc733bcb43a0
Mr.Stark <santosunny133@gmail.com>
2026-09-17 12:17:39 +0530

    fix: avoid bare except in docker check + plan parser (E722)

--- api.py
+++ api.py
@@ -39,7 +39,7 @@ def is_running_in_docker():
     try:
         with open('/proc/1/cgroup', 'r') as f:
             return 'docker' in f.read()
-    except:
+    except Exception:
         pass
 
     return False
